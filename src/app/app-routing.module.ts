5b3d53150a046dcbeb68b88ab486a88f21252828
Celia Sánchez Mariblanca <celiasanchez4ac@gmail.com>
2021-07-16 19:46:51 +0200

    crear componente principal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { PrincipalComponent } from './components/principal/principal.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'app', component: AppComponent},
+  {path: 'principal', component: PrincipalComponent},
+
+  //{ path: '**', redirectTo: 'app' } //Pagina por defecto
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
